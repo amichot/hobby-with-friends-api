6cfb62b908c0074b5fe7e97e4d96e0331b8f5611
amichot <30712077+amichot@users.noreply.github.com>
2019-07-18 19:40:38 -0500

    edit production url

--- src/config.js
+++ src/config.js
@@ -2,8 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL:
-    process.env.DB_URL ||
-    'postgresql://dunder-mifflin:1@localhost/hobby-with-friends',
+    'postgres://cxdihkbcgoyzpm:0c5807cf4a085d9e30cb3b9e2d476893aa946b9dedf65cc5ef6d0533fcca9286@ec2-107-20-168-237.compute-1.amazonaws.com:5432/daov4ss2k1umqk',
   JWT_SECRET:
     process.env.JWT_SECRET || 'change-this-secret',
   CLIENT_ORIGIN:
