31dd6459ae23e1e5de5a3fb300ba3ebe20cb2f99
amichot <30712077+amichot@users.noreply.github.com>
2019-07-18 20:07:49 -0500

    cors error

--- src/app.js
+++ src/app.js
@@ -18,9 +18,7 @@ const morganOption =
   NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
-app.use(cors({
-  origin: CLIENT_ORIGIN
-}));
+app.use(cors());
 app.use(helmet());
 
 app.use('/api/auth', authRouter);
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   DB_URL:
     'postgres://cxdihkbcgoyzpm:0c5807cf4a085d9e30cb3b9e2d476893aa946b9dedf65cc5ef6d0533fcca9286@ec2-107-20-168-237.compute-1.amazonaws.com:5432/daov4ss2k1umqk',
   JWT_SECRET:
